f14fcfc8611b1c3535537c6c5414cdaee9767af8
linuslange05 <linus.lange.work@gmail.com>
2024-07-15 00:25:49 +0200

    pushing again as DrawHandler files weren't tracked by git

---
+++ include/Scene/DrawHandler.h
@@ -0,0 +1,29 @@
+#ifndef REACTIONGAME_DRAWHANDLER_H
+#define REACTIONGAME_DRAWHANDLER_H
+
+#include "HelperClasses/Constants.h"
+#include "KittiObject.h"
+
+enum relativePosition{left, right, top, bottom};
+
+
+class DrawHandler {
+    cv::Mat img;
+public:
+    DrawHandler();
+    ~DrawHandler();
+
+    void drawPlayerMissedClick(int x, int y, KittiObject correctObj);
+
+    void drawPlayerClickedCorrect(int x, int y, KittiObject &correctObj);
+
+    void drawClickedPoint(int x, int y, cv::Scalar color);
+
+    void drawDistToCorrectBox(int x, int y, GTBoundingBox correctObj);
+
+    void setImg(const cv::Mat &img);
+
+
+};
+
+#endif //REACTIONGAME_DRAWHANDLER_H
